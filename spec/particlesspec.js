5dd1864ff2a1c3155fae7dfefdc059ca43fd0342
Truls Tangstad <tangstad@pvv.org>
2011-04-27 00:20:16 +0900

    Added after-the-fact experiment test.

--- spec/particlesspec.js
+++ spec/particlesspec.js
@@ -4,4 +4,19 @@ describe('Particle', function() {
 	expect(particle.w).toEqual(10);
 	expect(particle.h).toEqual(20);
     });
+
+    it('can draw itself as a red box to the canvas', function() {
+	var particle = new Particle(10, 20);
+	var Context = function() {};
+	var drawn = false;
+	Context.prototype.fillRect = function() {
+	    drawn = true;
+	};
+	var ctx = new Context();
+
+	particle.drawIt(ctx);
+
+	expect(ctx.fillStyle).toMatch('#[0-9a-f]00');
+	expect(drawn).toBeTruthy();
+    });
 });
