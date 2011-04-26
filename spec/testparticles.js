fdb2c6d830846a9b62e594d71bf0616d737f1d01
Truls Tangstad <tangstad@pvv.org>
2011-04-27 00:37:26 +0900

    Removed files not needed for site.

--- spec/testparticles.js
+++
@@ -1,8 +0,0 @@
-ParticleTest = TestCase("ParticleTest");
-
-ParticleTest.prototype.testParticle = function() {
-    var particle = new Particle(10, 10);
-    assertEquals(10, particle.w);
-    assertEquals(10, particle.h);
-}
-
